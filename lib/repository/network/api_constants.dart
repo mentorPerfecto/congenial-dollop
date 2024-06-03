1629ba75a26b72c664b6c308b60cd3549428e66f
MENTOR <odiichibueze7@gmail.com>
2024-06-03 09:58:15 +0100

    chore: code refactor

--- lib/repository/network/api_constants.dart
+++ lib/repository/network/api_constants.dart
@@ -1,14 +1,14 @@
 class ApiConstants {
- // static String establishConnectionUrl = "wss://fstream.binance.com:9443/ws";
+ //static String establishConnectionUrl = "wss://fstream.binance.com:9443/ws";
  static String establishConnectionUrl = "wss://testnet.binance.vision/ws";
-  static String symbolsUrl = "https://api.binance.com/api/v3/ticker/price";
+  static String symbolsUrl = "https://testnet.binance.vision/api/v3/ticker/price";
   static String symbolUrl(String symbol) =>
-      "https://api.binance.com/api/v3/ticker/24hr?symbol=$symbol";
+      "https://testnet.binance.vision/api/v3/ticker/24hr?symbol=$symbol";
   static String candlesUrl(
           {required String symbol, required String interval, int? endTime}) =>
-      "https://api.binance.com/api/v3/klines?symbol=$symbol&interval=$interval${endTime != null ? "&endTime=$endTime" : ""}";
+      "https://testnet.binance.vision/api/v3/klines?symbol=$symbol&interval=$interval${endTime != null ? "&endTime=$endTime" : ""}";
   static String orderBooksUrl({required String symbol, required int limit}) =>
-      "https://api.binance.com/api/v3/depth?symbol=$symbol&limit=$limit";
+      "https://testnet.binance.vision/api/v3/depth?symbol=$symbol&limit=$limit";
   static String exchangeInfoUrl(String symbol) =>
-      "https://api.binance.com/api/v3/exchangeInfo?symbol=$symbol";
+      "https://testnet.binance.vision/api/v3/exchangeInfo?symbol=$symbol";
 }
